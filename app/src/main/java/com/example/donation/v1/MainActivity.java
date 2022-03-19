bc1badd5d6bd2f1c7a2c878951bca535e29561f9
Your Name <you@example.com>
2022-03-19 09:39:28 +0700

    completed ex5

--- app/src/main/java/com/example/donation/v1/MainActivity.java
+++ app/src/main/java/com/example/donation/v1/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.donation.v1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MAX_DONATED_AMOUNT = 10000;
@@ -53,5 +57,28 @@ public class MainActivity extends AppCompatActivity {
 
         Log.v("Donate", "Donate Pressed " + amount + ", method: " + method);
         Log.v("Donate", "Current donated amount: " + totalDonated);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuReport:
+                Toast toast = Toast.makeText(this, "Report Selected", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+
+            default:
+                Toast toast1 = Toast.makeText(this, "Setting click", Toast.LENGTH_SHORT);
+                toast1.show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
